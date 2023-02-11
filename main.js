19b6fb2e859203430323057becd8f51a631fd632
Dididididii <1281627297@qq.com>
2023-02-11 13:22:40 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -2,10 +2,33 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {$http} from '@escook/request-miniprogram'
+
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = "https://www.uinav.com"
+// 请求开始之前做一些事情
+$http.beforeRequest = (options)=>{
+  uni.showLoading({
+    title:'数据加载中...'
+  })
+}
+// 请求完成之后做一些事情
+$http.afterRequest=()=>{
+  uni.hideLoading()
+}
+// 封装的展示消息提示的方法
+uni.$showMsg = (title='数据加载失败！',duration = 1500)=>{
+  uni.showToast({
+    title,
+    duration,
+    icon:'none'
+  })
+}
 
 const app = new Vue({
     ...App
